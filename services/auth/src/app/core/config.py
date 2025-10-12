12a029358291ed87bac7afaafff4b1089e66d4ef
Maniakalochka <prochorwolochoff@gmail.com>
2025-10-12 15:23:53 +0300

    add authentication settings to environment configuration

--- services/auth/src/app/core/config.py
+++ services/auth/src/app/core/config.py
@@ -4,12 +4,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
+    # database
     AUTH_DB_URL: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
     POSTGRES_HOST: str
     POSTGRES_PORT: str
+
+    # cryptography
+    SECRET: str
+    ALGORITHM: str
 
 
 settings = Settings()  # type: ignore
